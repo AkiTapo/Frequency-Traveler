bd586adfb51da55289fa1d90c3f7e416ff3ccc2a
lauris47 <lauris4747@gmail.com>
2017-04-04 13:00:05 +0200

    Parashute

--- Assets/Scripts/Boxes.cs
+++ Assets/Scripts/Boxes.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boxes : MonoBehaviour {
+public class Boxes : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), gameObject.GetComponent<Collider>(), true);
+            //GetComponent<Rigidbody>().drag = 10;
+        }
+        else
+        {
+            GetComponent<Rigidbody>().drag = 2;
+        }
+    }
+
 }
